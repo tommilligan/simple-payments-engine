3e888872db5a80a1ffacaa48c9953562bca5bad6
Tom Milligan <tom@reinfer.io>
2022-04-09 11:40:54 +0100

    lifted logic to above both stores

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,7 @@ pub mod state {
         use crate::action;
         use crate::TransferId;
         use std::collections::HashMap;
+        use thiserror::Error;
 
         #[derive(Debug, Clone)]
         // Make sure this is small, we need to store one per transfer.
@@ -66,98 +67,14 @@ pub mod state {
             pub status: Status,
         }
 
-        #[derive(Debug, Clone, Default)]
-        pub struct Store {
-            pub inner: HashMap<TransferId, State>,
-        }
-
-        use thiserror::Error;
-
-        #[derive(Error, Debug)]
-        pub enum Error {
-            #[error("not found: {transfer_id:?}")]
-            NotFound { transfer_id: TransferId },
-            #[error("conflict:  {transfer_id:?}, {kind}: {description}")]
-            Conflict {
-                transfer_id: TransferId,
-                kind: &'static str,
-                description: String,
-            },
-        }
-
-        pub type Result<T> = std::result::Result<T, Error>;
-
-        impl Store {
-            pub fn apply(&mut self, action: action::Action) -> Result<()> {
-                let action::Action { transfer_id, kind } = action;
-                match kind {
-                    action::ActionKind::Transfer(transfer) => {
-                        use std::collections::hash_map::Entry;
-                        match self.inner.entry(transfer_id) {
-                            Entry::Occupied(_) => {
-                                return Err(Error::Conflict {
-                                    transfer_id,
-                                    kind: "transfer exists",
-                                    description: "a transfer already exists with this id"
-                                        .to_owned(),
-                                })
-                            }
-                            Entry::Vacant(entry) => {
-                                entry.insert(State {
-                                    value: transfer.value,
-                                    status: Status::Transferred,
-                                });
-                            }
-                        }
-                    }
-                    action::ActionKind::Dispute(_) => {
-                        let transfer = self
-                            .inner
-                            .get_mut(&transfer_id)
-                            .ok_or_else(|| Error::NotFound { transfer_id })?;
-                        match transfer.status {
-                            Status::Transferred => transfer.status = Status::Disputed,
-                            _ => {
-                                return Err(Error::Conflict {
-                                    transfer_id,
-                                    kind: "disputed non-transferred transfer",
-                                    description: format!(
-                                        "transfer should be transferred, found: {:?}",
-                                        transfer.status
-                                    ),
-                                })
-                            }
-                        }
-                    }
-                    action::ActionKind::Close(_) => {
-                        let transfer = self
-                            .inner
-                            .get_mut(&transfer_id)
-                            .ok_or_else(|| Error::NotFound { transfer_id })?;
-                        match transfer.status {
-                            Status::Disputed => transfer.status = Status::Closed,
-                            _ => {
-                                return Err(Error::Conflict {
-                                    transfer_id,
-                                    kind: "closed non-disputed transfer",
-                                    description: format!(
-                                        "transfer should be disputed, found: {:?}",
-                                        transfer.status
-                                    ),
-                                })
-                            }
-                        }
-                    }
-                }
-                Ok(())
-            }
-        }
+        pub type Store = HashMap<TransferId, State>;
     }
 
     pub mod client {
-        use crate::ClientId;
+        use crate::{action, ClientId};
         use std::collections::HashMap;
 
+        #[derive(Debug, Clone)]
         // Make sure this is small, we need to store one per client.
         #[repr(u8)]
         pub enum Access {
@@ -165,6 +82,7 @@ pub mod state {
             Frozen,
         }
 
+        #[derive(Debug, Clone)]
         pub struct State {
             pub available: f64,
             pub held: f64,
@@ -181,8 +99,92 @@ pub mod state {
     }
 }
 
-use state::client::Store as ClientStore;
-use state::transfer::Store as TransferStore;
+use state::client;
+use state::transfer;
+use thiserror::Error;
+
+#[derive(Debug, Clone, Default)]
+pub struct Store {
+    client: client::Store,
+    transfer: transfer::Store,
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("not found: {transfer_id:?}")]
+    NotFound { transfer_id: TransferId },
+    #[error("conflict:  {transfer_id:?}, {kind}: {description}")]
+    Conflict {
+        transfer_id: TransferId,
+        kind: &'static str,
+        description: String,
+    },
+}
+
+impl Store {
+    pub fn apply(&mut self, action: action::Action) -> Result<(), Error> {
+        let action::Action { transfer_id, kind } = action;
+        match kind {
+            action::ActionKind::Transfer(payload) => {
+                use std::collections::hash_map::Entry;
+                match self.transfer.entry(transfer_id) {
+                    Entry::Occupied(_) => {
+                        return Err(Error::Conflict {
+                            transfer_id,
+                            kind: "transfer exists",
+                            description: "a transfer already exists with this id".to_owned(),
+                        })
+                    }
+                    Entry::Vacant(entry) => {
+                        entry.insert(transfer::State {
+                            value: payload.value,
+                            status: transfer::Status::Transferred,
+                        });
+                    }
+                }
+            }
+            action::ActionKind::Dispute(_) => {
+                let transfer = self
+                    .transfer
+                    .get_mut(&transfer_id)
+                    .ok_or_else(|| Error::NotFound { transfer_id })?;
+                match transfer.status {
+                    transfer::Status::Transferred => transfer.status = transfer::Status::Disputed,
+                    _ => {
+                        return Err(Error::Conflict {
+                            transfer_id,
+                            kind: "disputed non-transferred transfer",
+                            description: format!(
+                                "transfer should be transferred, found: {:?}",
+                                transfer.status
+                            ),
+                        })
+                    }
+                }
+            }
+            action::ActionKind::Close(_) => {
+                let transfer = self
+                    .transfer
+                    .get_mut(&transfer_id)
+                    .ok_or_else(|| Error::NotFound { transfer_id })?;
+                match transfer.status {
+                    transfer::Status::Disputed => transfer.status = transfer::Status::Closed,
+                    _ => {
+                        return Err(Error::Conflict {
+                            transfer_id,
+                            kind: "closed non-disputed transfer",
+                            description: format!(
+                                "transfer should be disputed, found: {:?}",
+                                transfer.status
+                            ),
+                        })
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
